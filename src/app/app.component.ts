9592d9bdc88b10a14a42208a54feb9da474f3800
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-08 11:14:29 +0300

    change the content to router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from './layout/layout.component';
 import { NgForOf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent, NgForOf],
+  imports: [LayoutComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
